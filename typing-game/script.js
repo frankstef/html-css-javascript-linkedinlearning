27306fdac82cd5e984e36a543e2e498d5068b21a
frankstef <famaddela@up.edu.ph>
2025-08-04 17:04:41 +0800

    feat: rendering of random text using api calls

--- typing-game/script.js
+++ typing-game/script.js
@@ -12,6 +12,7 @@ try {
 	const response = await fetch(url, options);
 	const result = await response.text();
 	console.log(result);
+    return result;
 } catch (error) {
 	console.error(error);
 }
@@ -31,15 +32,30 @@ const resetButton = document.getElementById('reset-button1');
 
 startButton.addEventListener('click', () => {
     console.log('Start button clicked');
+    startButton.innerText = 'LOADING...';
+    sentence().then((result) => {
+        result = result.replace(/^"|"$/g, '');
+        console.log(result);
+        document.getElementById('text-para1').innerText = result;
+        document.getElementById('input-area1').focus();
+    }).catch((error) => {
+        console.error('Error fetching sentence:', error);
+    }).then(() => {
+       startButton.innerText = 'START';
     document.querySelectorAll('.action-button').forEach(button => {
         button.classList.toggle('hidden');
     });
-
+    });
     
+
 })
 
 resetButton.addEventListener('click', () => {
     console.log('Reset button clicked');
+
+    document.getElementById('text-para1').innerText = 'Click "START" to begin the game';
+    document.getElementById('input-area1').value = '';
+
     document.querySelectorAll('.action-button').forEach(button => {
         button.classList.toggle('hidden');
     })
